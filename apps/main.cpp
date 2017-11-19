4003e193117851f2cf0420ad93915184ff291f67
YongBei.Ma <galowma@gmail.com>
2017-11-19 14:27:33 +0800

    homework1

---
+++ apps/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "src/args.h"
+
+int main(){
+	/** 1) use args
+		input : ./main --i inputfilename --o outputfilename --pixel_size 3.14
+		output : inputfilename,outputfilename,3.14
+	**/
+	std::cout<<"hello world."<<std::endl;
+	return 1;
+}
